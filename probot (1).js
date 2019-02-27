237dc183379610f367f480e7e8a7352beedfc0b0
Mehdi7894 <47418152+Mehdi7894@users.noreply.github.com>
2019-02-27 12:47:03 +0100

    Update probot (1).js

--- probot (1).js
+++ probot (1).js
@@ -1730,8 +1730,8 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 if(message.content.startsWith(prefix + "daily")) {
   if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
-    profile[message.author.id].credits += 22222222265555595
-     message.channel.send(`**${message.author.username} you collect your \`22222222265555595\` :dollar: daily pounds**`)
+    profile[message.author.id].credits += 1000000
+     message.channel.send(`**${message.author.username} you collect your \`1000000\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
